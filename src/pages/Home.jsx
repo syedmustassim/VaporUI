e7596ae1599d05ed6658112218057cecb2a9476f
Syed Mustassim <smustassim@gmail.com>
2023-09-11 17:48:48 +0530

    feat: home design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ import "./Home.css"
 const Home = () => {
     return(
         <div className="home-container">
-            <h1>VaporUI</h1>
+            <div className="home-main">
+                <img src="https://i.postimg.cc/qRhb58yj/vapor-one.jpg" alt="vapor" className="vapor-img"/>
+                <h1 className="home-heading">Vapor UI</h1>
+                <p className="home-content">Vapor-1 aims to simplify your design and development workflow, so you can create stunning, user-friendly experiences.</p>
+                <button className="home-btn">Get Started</button>
+            </div>
+
         </div>
     )
 }
